a1b430e061c3405ecf660dcb121f23161c7d0a8a
carl12 <carl.rodriguez13@gmail.com>
2015-06-08 09:49:50 -0700

    initial commit - fixed bug

--- Shallow_sea_sim.m
+++ Shallow_sea_sim.m
@@ -45,12 +45,12 @@ ObsValuesH = importdata('OBS_matrix_H.mat','-mat');
 [~,Nvar,~] = size(ObsValuesH);
 
 
-num_elems =1; % for now set state elements to 1, will be 3 in the end
+num_elems =4096; % for now set state elements to 1, will be 3 in the end
 
 % Observation mapping operator, H is a fat short matrix
 
-Obs = 1 : Nvar; % Observe all, can change step size to alter frequency of observations
-H_Map = zeros(num_elems, (Nvar)^2);
+Obs = 1 : num_elems; % Observe all, can change step size to alter frequency of observations
+H_Map = zeros(num_elems, num_elems);
 
 for i = 1 : num_elems
     H_Map(i, Obs(i)) = 1;
@@ -69,8 +69,8 @@ drop_dim = 21;
 D = zeros(21,21,Nens);  % create empty array for different drops
 
 for i = 1 : Nens
-    a = 1.4;                  % min size
-    b = 3;                  % max size
+    a = 1.5;                  % min size
+    b = 2.5;                  % max size
     height = (b-a).*randn(1,1) + a;   % initial drop size
     
     D(:,:,i) = droplet(height,drop_dim);     % simulate a water drop (size,???)
@@ -108,18 +108,7 @@ for itime = 1 : time
         fprintf('Current run: %d \n',itime)
     end
     
-    % Observations at the end of the time interval
-    z = squeeze(ObsValuesH(itime, :,:))';
     
-    % measurement error and covariance
-    gama = 0 + 1.*randn(num_elems, Nens);   % Gaussian observation perturbation, Generate values from a normal distribution with mean 0 and standard deviation 1.
-    Obs_cov = (gama * gama') / (Nens - 1);
-    Obs_ens = zeros(num_elems, Nens);
-    
-    % perturbed measurement
-    for i = 1 : num_elems
-        Obs_ens(i, :) = z(i) + gama(i, :);    %% Measurement Ensemble
-    end
     
     
     % Nested loop for Ensembles
@@ -205,7 +194,7 @@ for itime = 1 : time
         
         
         
-      
+        
         
         %% Update plot
         if mod(k,Nens) == 0
@@ -222,66 +211,100 @@ for itime = 1 : time
     %% Control observations with if loop and mod num
     if mod(itime,obs_freq) == 0, %(itime > 160))
         
-         
+        
+        % Observations at the end of the time interval
+        z = squeeze(ObsValuesH(itime, :,:))';
+        z = squeeze(reshape(z,4096,1));
+        size(z);
+        % measurement error and covariance
+        gama = zeros(4096); %+ .01*randn(num_elems, Nens);   % Gaussian observation perturbation, Generate values from a normal distribution with mean 0 and standard deviation 1.
+        gama = squeeze(gama);
+        Obs_cov = (gama * gama') / (Nens - 1);
+        Obs_ens = zeros(num_elems, Nens);
+        
+        
+        % perturbed measurement
+        for i = 1 : Nens
+            Obs_ens(:,i) = z + gama(1);    %% Measurement Ensemble
+        end
+        
+        Obs_ens;
+        
+        
+        asdf = 0
         % Compute ensemble
         
         OneN(1 : Nens, 1 : Nens) = 1 / Nens;
+        Hpre = H;
         H = permute(H,[1 2 3]);
         H = reshape(H(2:65,2:65,:),(Nvar)^2,Nens);
         %H_temp = reshape(H,Nvar+2,Nvar+2,Nens);
         Hbar   = H * OneN; % can't use three dimensions
         Hprime = H - Hbar;
-        
+        size(Hprime)
+        asdf1 = 0
         % Compute ensemble covariance matrix
-        Ens_cov = (Hprime * Hprime') / (Nens - 1);
-        
-        M = H_Map * Ens_cov * H_Map' + Obs_cov;   % Analysis equation
-        
+        %Ens_cov = (Hprime * Hprime') / (Nens - 1);
+        asdf2 = 0
+        %M = Ens_cov + Obs_cov;   % Analysis equation
+        %size(M)
+        asdf3 = 0
         % Compute M inverse
         %handling the singular values
         %M
-        [Uni_mat_U, S, Uni_mat_V] = svd(M);   % single value decomposition
-        Xi = diag(S);    % singular values
-        nos = length(Xi);
         
-        for jj = 1 : nos
-            if (Xi(jj) < Xi(1) / (10^6))
-                Xi(jj) = 0;
-            else
-                Xi(jj) = 1 / Xi(jj);
-            end
-        end
-        
-        S = diag(Xi);
-        
-        mInverse = Uni_mat_V * S * Uni_mat_U';    % M inverse = V * inv(S) * U';
+%         [Uni_mat_U, S, Uni_mat_V] = svd(M);   % single value decomposition
+%         Xi = diag(S);% singular values
+%         asdf3_4 = 0
+%         nos = length(Xi);
+%         asdf3_5 = 0
+%         for jj = 1 : nos
+%             if (Xi(jj) < Xi(1) / (10^6))
+%                 Xi(jj) = 0;
+%             else
+%                 Xi(jj) = 1 / Xi(jj);
+%             end
+%         end
+%         asdf4 = 0
+%         S = diag(Xi);
+%         
+%         mInverse = Uni_mat_V * S * Uni_mat_U';    % M inverse = V * inv(S) * U';
         
         % Data Assimilate
-        H = H + Ens_cov * H_Map' * ( mInverse * (Obs_ens - H_Map * H) );
-        %H = H + H_Map' * (Obs_ens - H_Map * H);
-        
+        %H = H + Ens_cov * H_Map' * ( mInverse * (Obs_ens - H_Map * H) );
+        H = H + (Obs_ens - H_Map * H);
+        asdf5= 0
+        size(Obs_ens)
+        size(H_Map)
         % Reshape H back to 3 dimensions!!!
         H = reshape(H,Nvar,Nvar,Nens);
         
         B = zeros(Nvar+2,Nvar+2,Nens);
         B(2:Nvar+1,2:Nvar+1,:)= H;
         H = B;
-        
+        asdf = H - Hpre;
+        for x = 1:66
+            for y = 1:66
+                if asdf > 1
+                    fprintf('x: %d, y: %d',asdf(x,y))
+                end
+            end
+        end
         
     end
-      %% Calc Error
-        for q = 1:Nvar
-            for p = 1:Nvar
-                for s = 1 : Nens
-%                     if abs((H(q+1,p+1,s)-ObsValuesH(itime,q,p))) > 0.5
-%                         (H(q+1,p+1,s)-ObsValuesH(itime,q,p))
-%                     end
-                    temp = temp +  (H(q+1,p+1,s)-ObsValuesH(itime,q,p))^2;
-                end
-                RMS_H(itime,q,p) = sqrt(temp/Nens);
-                temp = 0;
+    %% Calc Error
+    for q = 1:Nvar
+        for p = 1:Nvar
+            for s = 1 : Nens
+                %                     if abs((H(q+1,p+1,s)-ObsValuesH(itime,q,p))) > 0.5
+                %                         (H(q+1,p+1,s)-ObsValuesH(itime,q,p))
+                %                     end
+                temp = temp +  (H(q+1,p+1,s)-ObsValuesH(itime,q,p))^2;
             end
-        end  
+            RMS_H(itime,q,p) = sqrt(temp/Nens);
+            temp = 0;
+        end
+    end
 end
 disp('Run time.....');
 toc;
